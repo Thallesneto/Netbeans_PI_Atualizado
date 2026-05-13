25216b4afc46890ba365c08123e074e8f29226d8
Thalles <thallessalles042@gmail.com>
2026-05-13 18:36:35 -0300

    teste

---
+++ PI/src/main/java/com/mycompany/pi/PI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pi;
+
+/**
+ *
+ * @author Roberto
+ */
+public class PI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ projeto/src/main/java/com/mycompany/projeto/Projeto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projeto;
+
+/**
+ *
+ * @author 26.00040-6
+ */
+public class Projeto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
